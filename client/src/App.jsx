1fa810dc750b628fddd57e438ebecf7a6ac066e8
meherab151175 <meherabm803@gmail.com>
2024-08-29 17:02:49 +0600

    Header component added and modification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Login from './Login'
 import Register from './Register'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+
+export default function Header() {
+  return (
+    <div className="bg-slate-200">
+        <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+            <Link to="/">
+                <h1 className="font-bold">Auth App</h1>
+            </Link>
+            <ul className="flex">
+                <Link to="/">
+                    <li>Home</li>
+                </Link>
+                <Link to="/about">
+                    <li>About</li>
+                </Link>
+                <Link to="/sign-in">
+                    <li>Sign in</li>
+                </Link>
+            </ul>
+        </div>
+    </div>
+  )
+}
